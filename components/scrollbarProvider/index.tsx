19e64d3bdc3ec6967b788d913320776843ff08ca
dmitriikushnir <student95tnu@gmail.com>
2026-06-27 12:32:46 +0300

    smooth scroll added

---
+++ components/scrollbarProvider/index.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import Scrollbar from "smooth-scrollbar";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function SmoothScrollbarProvider({ children }: Props) {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!containerRef.current) return;
+
+    const scrollbar = Scrollbar.init(containerRef.current);
+
+    return () => {
+      scrollbar.destroy();
+    };
+  }, []);
+
+  return (
+    <div
+      ref={containerRef}
+      style={{
+        height: "100vh",
+      }}
+    >
+      {children}
+    </div>
+  );
+}
