9724b0f9eeafc0f20adc4f09c9279702d17159c9
dmitriikushnir <student95tnu@gmail.com>
2026-06-29 13:56:53 +0300

    close button added

--- components/sections/form/index.tsx
+++ components/sections/form/index.tsx
@@ -2,8 +2,10 @@
 import Button from "@/components/ui/button";
 import styles from "./index.module.scss";
 import { useEffect, useState } from "react";
+import Image from "next/image";
 import Input from "@/components/ui/input";
 import MaskedInput from "@/components/ui/maskedInput";
+import CloseIcon from "@/public/CloseIcon.svg";
 
 type Props = {
   closeForm: VoidFunction;
@@ -47,47 +49,49 @@ export default function Form({ closeForm }: Props) {
       onAnimationEnd={handleAnimationEnd}
       className={`${styles.section} ${closing ? styles.closing : ""}`}
     >
-      <div style={{ height: "40px" }}>
-        <Button onClick={() => setClosing(true)}>Закрыть</Button>
+      <div className={styles.closeButton} onClick={() => setClosing(true)}>
+        <Image src={CloseIcon} fill priority alt={"Close"} />
       </div>
-      <h2>ЗАКАЗАТЬ ЗВОНОК</h2>
-      <form className={styles.form} onSubmit={handleSubmit}>
-        <Input
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          required
-          name="name"
-          label="Ваше Имя"
-        ></Input>
-        <MaskedInput
-          label="Телефон"
-          type="tel"
-          mask="+{7} (000) 000-00-00"
-          placeholder="+7 (___) ___-__-__"
-          onAccept={(value) => setPhone(value)}
-          required
-          name="phoneNumber"
-        />
-        <Input
-          label="Email"
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-          name="email"
-        />
-        <p className={styles.notice}>
-          Нажимая на кнопку «Отправить», вы ознакомлены и подтверждаете согласие
-          с{" "}
-          <a className={styles.noticeLink} href="#">
-            политикой обработки персональных данных
-          </a>
-        </p>
-        <Button size="xl" type="submit" className={styles.button}>
-          ОТПРАВИТЬ
-        </Button>
-      </form>
+      <div className={styles.formContainer}>
+        <h1 className={styles.heading}>ЗАКАЗАТЬ ЗВОНОК</h1>
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <Input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+            name="name"
+            label="Ваше Имя"
+          ></Input>
+          <MaskedInput
+            label="Телефон"
+            type="tel"
+            mask="+{7} (000) 000-00-00"
+            placeholder="+7 (___) ___-__-__"
+            onAccept={(value) => setPhone(value)}
+            required
+            name="phoneNumber"
+          />
+          <Input
+            label="Email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            name="email"
+          />
+          <p className={styles.notice}>
+            Нажимая на кнопку «Отправить», вы ознакомлены и подтверждаете
+            согласие с{" "}
+            <a className={styles.noticeLink} href="#">
+              политикой обработки персональных данных
+            </a>
+          </p>
+          <Button size="xl" type="submit" className={styles.button}>
+            ОТПРАВИТЬ
+          </Button>
+        </form>
+      </div>
     </section>
   );
 }
